afbe0fc66ef8f2599e62099f3069eff8d1f94bd5
Kliuchnikava <yana.kliuch@gmail.com>
2023-06-15 08:08:30 +0200

    fix-build
    fix build + remove console statements

--- src/client/DatasetsApiClient.ts
+++ src/client/DatasetsApiClient.ts
@@ -4,14 +4,12 @@ import { type Dataset, type DatasetDetail} from './DatasetsApiClient.types';
 // TODO: fix from BE -> need to return array of objects - asked Dan
 export async function getAvailableDatasets(): Promise<Dataset[]> {
     return httpClient.get('/api/v1/datasets').then(response  => {
-        console.log('resp', JSON.stringify(response.data));
         return response.data;
     });
 }
 
 export async function getDatasetSampleData(datasetId: string): Promise<DatasetDetail> {
     return httpClient.get(`/api/v1/datasets/${datasetId}`).then(response => {
-        console.log('resp', JSON.stringify(response.data));
         return response.data;
     });
 }
