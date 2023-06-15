d2d25a1e765e971d7c3974cb9ee1789997ffdb4c
KlYana <yana.kliuch@gmail.com>
2023-06-15 13:19:01 +0200

    WIP remove syntable

--- src/components/datasetDetail/DatasetDetailSampleDataTable.vue
+++ src/components/datasetDetail/DatasetDetailSampleDataTable.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="border border-solid rounded-2xl p-8 mt-8">
     <h3 class="mb-8 text-2xl font-medium">Sample segments</h3>
-    <SynTable
-      :table-header-props="tableHeaderProps"
-      :full-width="true"
-      :table-body-props="tableBodyProps"
-    />
+    <table>
+      <thead>
+        <tr>
+          <th>Source</th>
+          <th>Target</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="segment.id" v-for="segment in datasetStore.selectedDatasetSample.segments">
+          <td>
+            {{ segment.source }}
+          </td>
+          <td>
+            {{ segment.target }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
