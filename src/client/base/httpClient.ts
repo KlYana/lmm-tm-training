79cd906cda1da55e2cd633068780c809ead7f082
KlYana <yana.kliuch@gmail.com>
2023-06-15 14:00:02 +0200

    add base url

--- src/client/base/httpClient.ts
+++ src/client/base/httpClient.ts
@@ -1,23 +1,24 @@
-import axios, { type AxiosRequestConfig } from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios'
 
-let csrfToken: string;
+let csrfToken: string
 
 export const getCsrfToken = (): string => {
-    if (!csrfToken) {
-        csrfToken = (document.getElementsByName("__RequestVerificationToken")[0] as HTMLInputElement)?.value || "";
-    }
-    return csrfToken;
-};
+  if (!csrfToken) {
+    csrfToken =
+      (document.getElementsByName('__RequestVerificationToken')[0] as HTMLInputElement)?.value || ''
+  }
+  return csrfToken
+}
 
 const config: AxiosRequestConfig = {
-    baseURL: "",
-};
+  baseURL: 'https://phrase-hackathon-llm-tm.azurewebsites.net'
+}
 
-const httpClient = axios.create(config);
+const httpClient = axios.create(config)
 
 httpClient.interceptors.response.use(undefined, (error) => {
-    console.error("API ERROR", error);
-    return Promise.reject(error);
-});
+  console.error('API ERROR', error)
+  return Promise.reject(error)
+})
 
-export { httpClient };
+export { httpClient }
